231c27d6ca227c37267395b20c86b81c1a56e9ee
Jihoon Kim <j5kim@ucsd.edu>
2022-01-21 14:44:21 -0800

    replace docker ubuntu with scipy/scipy-dev

--- week04/appendix_install_on_docker_from_scratch.sh
+++ week04/appendix_install_on_docker_from_scratch.sh
@@ -14,29 +14,29 @@ pwd
 #    For example,  -v /users/JohnDoe:/workspace
 # 2) you can give your own name for the docker in place of med263_bansal
 #    For example,  --name med263_week4
-docker run --name med263_bansal -v $(PWD):/workspace -ti scipy/scipy-dev /bin/bash
+docker run --name med263_bansal -v $(PWD):/workspace -ti ubuntu /bin/bash
 
 
 ###---------------------------------------------------------------------------
 ### RUN INSIDE DOCKER CONTAINER
 ###---------------------------------------------------------------------------
-# switch user from 'gitpod' to 'root'
-sudo -i -u root
-
-# check the current user
-whoami
-
-# update linux packages
+# install python3
 apt-get update
+apt-get install -y software-properties-common gcc 
+add-apt-repository -y ppa:deadsnakes/ppa
+apt-get update
+apt-get install -y python3.8 python3-distutils python3-pip python3-apt
+python3 --version
+
+# install python3 packages
+pip install numpy
+pip install scipy
+
+# install git
+apt-get install -y git
 
 # install tabix
 apt-get install  -y tabix
-
-# swith user back to 'gitpod' from 'root'
-exit
-
-# check the current user
-whoami
 
 # change the working directory to /workspace
 cd /workspace
